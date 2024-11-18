2deacb6f4271fd7cd7d43a45c0b8114eecb23ab3
hanbyul0103 <all007b@naver.com>
2024-11-18 23:29:49 +0900

    fix: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const config = require('./config/config.json');
 const { Client, IntentsBitField } = require('discord.js');
 
-let currentGuild;
-let firstGradeRole, secondGradeRole, thirdGradeRole, student, graduate;
-let gradeRoles;
 let members;
 let ownerId;
-const batchSize = 30; // 전역 변수로 배치 크기 설정
+const serverRoles = new Map(); // 서버별 역할 저장용
 
 const client = new Client({
     intents: [
@@ -19,122 +16,151 @@ const client = new Client({
 
 client.once('ready', async () => {
     console.log(`[!] Bot Login: ${client.user.tag}`);
-
-    for (const guild of client.guilds.cache.values()) {
-        currentGuild = guild;
-        await GetRoles();
-        members = await currentGuild.members.fetch();
-        ownerId = guild.ownerId;
-    }
 });
 
 client.on('messageCreate', async message => {
-    if (message.author.bot) return;
+    if (message.author.bot || !message.guild) return;
 
     if (!message.content.startsWith(config.prefix)) return;
 
     const args = message.content.slice(config.prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === 'hi') {
-        await message.reply('hi');
+    const currentGuild = message.guild; // 현재 명령어를 보낸 서버
+    ownerId = currentGuild.ownerId;
+
+    if (command === 'init') {
+        await SaveMembers(currentGuild, message);
+        await GetRoles(currentGuild, message);
     }
 
     if (command === 'role') {
-        await RoleAssignment(message);
+        await RoleAssignment(currentGuild, message);
     }
 
     if (command === 'change') {
-        await ChangeNickname(message);
+        await ChangeNickname(currentGuild, message);
+    }
+
+    if (command === 'debug') {
+        await DebugRole(currentGuild, message);
+    }
+
+    if (command === 'remove') {
+        await RemoveDebugRole(currentGuild, message);
     }
 });
 
-async function GetRoles() {
+async function SaveMembers(currentGuild, message) {
+    members = await currentGuild.members.fetch();
+    message.reply("멤버 초기화 완료");
+    console.log(`Members fetched for guild: ${currentGuild.name}`);
+}
+
+async function GetRoles(currentGuild, message) {
     try {
-        firstGradeRole = currentGuild.roles.cache.find(role => role.name === '1학년');
-        secondGradeRole = currentGuild.roles.cache.find(role => role.name === '2학년');
-        thirdGradeRole = currentGuild.roles.cache.find(role => role.name === '3학년');
-        student = currentGuild.roles.cache.find(role => role.name === "재학생");
-        graduate = currentGuild.roles.cache.find(role => role.name === "졸업생");
+        const roles = {
+            firstGradeRole: currentGuild.roles.cache.find(role => role.name === '1학년'),
+            secondGradeRole: currentGuild.roles.cache.find(role => role.name === '2학년'),
+            thirdGradeRole: currentGuild.roles.cache.find(role => role.name === '3학년'),
+            student: currentGuild.roles.cache.find(role => role.name === "재학생"),
+            graduate: currentGuild.roles.cache.find(role => role.name === "졸업생"),
+            debugRole: currentGuild.roles.cache.find(role => role.name === "debug"),
+        };
 
-        gradeRoles = [firstGradeRole, secondGradeRole, thirdGradeRole, graduate];
+        roles.gradeRoles = [
+            roles.firstGradeRole,
+            roles.secondGradeRole,
+            roles.thirdGradeRole,
+            roles.graduate,
+        ];
 
-        console.log('Get roles done');
-    }
-    catch (error) {
-        console.error(`Error fetching roles: ${error}`);
+        serverRoles.set(currentGuild.id, roles);
+
+        message.reply(`${currentGuild.name} 서버의 역할이 저장되었습니다.`);
+    } catch (error) {
+        console.error(`역할을 불러오는 중 오류 발생 ${currentGuild.name}:`, error);
     }
 }
 
-async function RoleAssignment(message) {
-    const memberList = Array.from(members.values());
-
-    for (let i = 0; i < memberList.length; i += batchSize) {
-        const batch = memberList.slice(i, i + batchSize);
-        const rolePromises = batch.map(ProcessMemberRole);
-        await Promise.all(rolePromises);
-        await delay(1000); // 1초 지연
+async function RoleAssignment(currentGuild, message) {
+    const roles = serverRoles.get(currentGuild.id);
+    if (!roles) {
+        return message.reply("역할이 초기화되지 않았습니다. `init` 명령을 사용하세요.");
     }
 
-    await message.reply("부여 완료");
+    const memberList = Array.from(members.values()).filter(member => !member.user.bot);
+
+    for (const member of memberList) {
+        await ProcessMemberRole(member, roles).catch(error =>
+            console.log(`역할을 부여하던 중 오류 발생 ${member.user.tag}`)
+        );
+        await delay(3); // 3ms 대기
+    }
+
+    await message.reply("역할 부여 작업 완료");
 }
 
-async function ChangeNickname(message) {
+async function ChangeNickname(currentGuild, message) {
+    const roles = serverRoles.get(currentGuild.id);
+    if (!roles) {
+        return message.reply("역할이 초기화되지 않았습니다. `init` 명령을 사용하세요.");
+    }
+
     const memberList = Array.from(members.values())
         .filter(member => member.id !== ownerId);
 
-    for (let i = 0; i < memberList.length; i += batchSize) {
-        const batch = memberList.slice(i, i + batchSize);
-        const nicknamePromises = batch.map(ProcessNicknameChange);
-        await Promise.all(nicknamePromises);
-        await delay(1000); // 1초 지연
+    for (const member of memberList) {
+        await ProcessNicknameChange(member, roles);
+        await delay(3); // 3ms 대기
     }
 
     await message.reply('변경 완료');
 }
 
-async function ProcessMemberRole(member) {
+async function ProcessMemberRole(member, roles) {
     const nickname = member.nickname || member.user.username;
 
     try {
-        if (!member.roles.cache.has(student.id)) {
-            await member.roles.add(student);
+        if (!member.roles.cache.has(roles.student.id)) {
+            await member.roles.add(roles.student);
+            console.log(`Added '${roles.student.name}' role to ${member.user.tag}`);
         }
 
         if (nickname.includes('1학년')) {
-            await UpdateRoles(member, firstGradeRole);
+            await UpdateRoles(member, roles.firstGradeRole, roles.gradeRoles);
         } else if (nickname.includes('2학년')) {
-            await UpdateRoles(member, secondGradeRole);
+            await UpdateRoles(member, roles.secondGradeRole, roles.gradeRoles);
         } else if (nickname.includes('3학년')) {
-            await UpdateRoles(member, thirdGradeRole);
+            await UpdateRoles(member, roles.thirdGradeRole, roles.gradeRoles);
         }
     } catch (error) {
         console.error(`Error processing role for ${member.user.tag}:`, error);
     }
 }
 
-async function ProcessNicknameChange(member) {
+async function ProcessNicknameChange(member, roles) {
     const nickname = member.nickname || member.user.username;
     const name = nickname.split(' ')[0];
 
     try {
         if (nickname.includes('1학년')) {
             await member.setNickname(`${name} (2학년)`);
-            await UpdateRoles(member, secondGradeRole);
+            await UpdateRoles(member, roles.secondGradeRole, roles.gradeRoles);
         } else if (nickname.includes('2학년')) {
             await member.setNickname(`${name} (3학년)`);
-            await UpdateRoles(member, thirdGradeRole);
+            await UpdateRoles(member, roles.thirdGradeRole, roles.gradeRoles);
         } else if (nickname.includes('3학년')) {
             await member.setNickname(`${name} (졸업생)`);
-            await member.roles.remove(student);
-            await UpdateRoles(member, graduate);
+            await member.roles.remove(roles.student);
+            await UpdateRoles(member, roles.graduate, roles.gradeRoles);
         }
     } catch (error) {
         console.error(`Error changing nickname for ${member.user.tag}:`, error);
     }
 }
 
-async function UpdateRoles(member, targetRole) {
+async function UpdateRoles(member, targetRole, gradeRoles) {
     try {
         if (!member.roles.cache.has(targetRole.id)) {
             await member.roles.add(targetRole);
@@ -150,6 +176,30 @@ async function UpdateRoles(member, targetRole) {
     } catch (error) {
         console.error('Error updating roles for', member.user.tag, ':', error);
     }
+}
+
+async function DebugRole(currentGuild, message) {
+    const memberList = Array.from(members.values())
+        .filter(member => member.id !== ownerId);
+
+    for (const member of memberList) {
+        await member.roles.add(serverRoles.get(currentGuild.id).debugRole);
+        await delay(3); // 3ms 대기
+    }
+
+    await message.reply('디버그 역할 추가');
+}
+
+async function RemoveDebugRole(currentGuild, message) {
+    const memberList = Array.from(members.values())
+        .filter(member => member.id !== ownerId);
+
+    for (const member of memberList) {
+        await member.roles.remove(serverRoles.get(currentGuild.id).debugRole);
+        await delay(3); // 3ms 대기
+    }
+
+    await message.reply('디버그 역할 삭제');
 }
 
 function delay(ms) {
