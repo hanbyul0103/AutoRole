7444bb5d562e85d6b6cb9066b1a78369060d7038
hanbyul0103 <all007b@naver.com>
2025-10-30 12:28:42 +0900

    fix: embed generator can use default title and desc

---
+++ src/utils/embedGenerator.js
@@ -0,0 +1,24 @@
+import { EmbedBuilder } from "discord.js";
+
+function createEmbed({ title, description, fields = [], timestamp = false }) {
+    const embed = new EmbedBuilder()
+        .setTitle(title ?? " ")
+        .setDescription(description ?? " ");
+
+    if (fields.length > 0) {
+        embed.addFields(fields.map(f => ({
+            name: f.name,
+            //value: `\`\`\`diff\n${f.value} \n\`\`\``,
+            value: `${f.value}\n`,
+            inline: f.inline || false
+        })));
+    }
+
+    if (timestamp) {
+        embed.setTimestamp();
+    }
+
+    return embed;
+}
+
+export { createEmbed };
